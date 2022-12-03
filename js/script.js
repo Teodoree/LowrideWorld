e3abbca9235dc33b6b27f6126a66edd9c070b9fe
Teodoree <97417912+Teodoree@users.noreply.github.com>
2022-12-04 00:14:51 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const splide = new Splide('#splide1', {
+if (document.documentElement.clientWidth > 768) {
+	const splide = new Splide('#splide1', {
     type: 'loop',
     drag: 'free',
     focus: 'center',
@@ -9,7 +10,10 @@ const splide = new Splide('#splide1', {
         speed: 1,
     },
 });
-const splide2 = new Splide('#splide2', {
+	splide.mount(window.splide.Extensions);
+};
+if (document.documentElement.clientWidth > 768) {
+	const splide2 = new Splide('#splide2', {
 	type: 'loop',
     drag: 'free',
     focus: 'center',
@@ -20,6 +24,38 @@ const splide2 = new Splide('#splide2', {
         speed: -1,
     },
 });
+	splide2.mount(window.splide.Extensions);
+};
 
-splide.mount(window.splide.Extensions);
-splide2.mount(window.splide.Extensions);
+if (document.documentElement.clientWidth < 768) {
+	const splide = new Splide('#splide1', {
+    type: 'loop',
+    drag: 'free',
+    focus: 'center',
+    perPage: 2,
+    arrows: false,
+    pagination: false,
+    autoScroll: {
+        speed: 1,
+    },
+});
+	splide.mount(window.splide.Extensions);
+};
+if (document.documentElement.clientWidth < 768) {
+	const splide2 = new Splide('#splide2', {
+	type: 'loop',
+    drag: 'free',
+    focus: 'center',
+    perPage: 2,
+    arrows: false,
+    pagination: false,
+    autoScroll: {
+        speed: -1,
+    },
+});
+	splide2.mount(window.splide.Extensions);
+};
+
+
+
+
