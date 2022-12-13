ab5001ae5f09e43ef3f54e3088570b15c0818c97
Teodoree <97417912+Teodoree@users.noreply.github.com>
2022-12-14 01:57:16 +0600

    Add files via upload

--- js/scripthumburger.js
+++ js/scripthumburger.js
@@ -2,16 +2,19 @@ window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.nav-menu'),
     menuItem = document.querySelectorAll('.nav-menu__list-item'),
     hamburger = document.querySelector('.humburger');
+    span = document.querySelectorAll('.spans');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('humburger_active');
         menu.classList.toggle('nav-menu_active');
+
     });
 
-    menuItem.forEach(item => {
+    span.forEach(item => {
         item.addEventListener('click', () => {
-            hamburger.classList.toggle('humburger_active');
-            menu.classList.toggle('nav-menu_active');
+            hamburger.classList.remove('humburger_active');
+            menu.classList.remove('nav-menu_active');
         })
-    })
+    });
+
 })
